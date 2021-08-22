473909aa25791fceaad2e07996d88c5f87012e73
Paulo Alexandre Mello <palexandremello@gmail.com>
2021-08-22 18:35:30 -0300

    feat: shopping app

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -45,6 +45,17 @@ class ProductItem extends StatelessWidget {
           trailing: IconButton(
             icon: Icon(Icons.shopping_cart),
             onPressed: () {
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text("Produto adicionado com sucesso!"),
+                duration: Duration(seconds: 2),
+                action: SnackBarAction(
+                  label: 'DESFAZER',
+                  onPressed: () {
+                    cart.removeSingleItem(product.id);
+                  },
+                ),
+              ));
               cart.addItem(product);
             },
             color: Theme.of(context).accentColor,
